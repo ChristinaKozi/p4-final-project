28574f01c1cee13704ab3a68ac48ff5900e5743d
Christina Kozanian <christinakozanian@gmail.com>
2024-04-11 23:02:45 -0700

    completed signup feature

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -4,11 +4,38 @@ import { Box, Button, Error, FormField, Input, Label, Textarea } from "../styles
 function SignUpForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
     const [errors, setErrors] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setIsLoading(true);
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                passwordConfirmation: passwordConfirmation
+            }),
+        }).then((r)=> {
+            console.log(r)
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => console.log(user));
+            } else {
+                r.json().then((err)=> setErrors(err.errors));
+            }
+        });
+    }
+        
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <FormField>
                 <Label>Username:</Label>
                 <Input
@@ -22,12 +49,22 @@ function SignUpForm() {
             <FormField>
                 <Label>Password:</Label>
                 <Input
-                    type='text'
-                    id='username'
+                    type='password'
+                    id='password'
                     placeholder="Password"
                     value={password}
-                    onChange={(e)=>{setPassword(e.target.value)}}>
-                </Input>
+                    onChange={(e)=>{setPassword(e.target.value)}} 
+                />
+            </FormField>
+            <FormField>
+                <Label>Password Confirmation: </Label>
+                <Input
+                    type="password"
+                    id="password_confirmation"
+                    placeholder="Re-Enter Password"
+                    value={passwordConfirmation}
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                />
             </FormField>
             <FormField>
                 <Button type='submit'>{isLoading ? "Loading..." : "Sign Up"}</Button>
--- client/src/styles/Input.js
+++ client/src/styles/Input.js
@@ -5,8 +5,8 @@ const Input = styled.input`
   border: 1px solid transparent;
   border-color: #dbdbdb;
   -webkit-appearance: none;
-  max-width: 15%;
-  width: 100%;
+  max-width: 100%;
+  width: 40%;
   font-size: 1rem;
   line-height: 1.5;
   padding: 4px;
